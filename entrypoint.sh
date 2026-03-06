f53a469e2b9fad77907075c50f830c4844b2378f
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2026-03-06 01:38:05 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+source /opt/ros/jazzy/setup.bash
+
+# Launch ur_robot_driver connecting to URSim
+ros2 launch ur_robot_driver ur_control.launch.py \
+    ur_type:=${UR_TYPE:-ur5e} \
+    robot_ip:=${ROBOT_IP} \
+	reverse_ip:=172.20.0.3 \
+    launch_rviz:=false \
+    initial_joint_controller:=scaled_joint_trajectory_controller
