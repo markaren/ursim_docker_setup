3547b8476d73409894ad82dd27159ad2a773afc8
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2026-03-07 20:30:49 +0100

    refactor

--- docker_files/driver_entrypoint.sh
+++ docker_files/driver_entrypoint.sh
@@ -4,7 +4,7 @@ source /opt/ros/jazzy/setup.bash
 
 # Launch ur_robot_driver connecting to URSim
 ros2 launch ur_robot_driver ur_control.launch.py \
-    ur_type:=${UR_TYPE:-ur5e} \
+    ur_type:=${UR_TYPE} \
     robot_ip:=${ROBOT_IP} \
 	reverse_ip:=172.20.0.3 \
     launch_rviz:=false \
